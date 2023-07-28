b246f455c61ed0b3b00cc4991d72b848f1d4fbeb
Debatosh Roychowdhury <debatoshofficial85@gmail.com>
2023-07-29 03:23:26 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Class 12 Batch',
+        productNumber: '20231202',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Class 11 Batch',
+        productNumber: '20231103',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'WBJEE Batch',
+        productNumber: '202312003',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
